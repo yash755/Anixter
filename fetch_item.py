31ead8c9c51f04fa7df4b1a737e1be344d5ac836
YASH GUPTA <yashgupta@YASHs-Air.Dlink>
2018-12-11 13:01:51 +0530

    Change

--- fetch_item.py
+++ fetch_item.py
@@ -49,7 +49,7 @@ def get_list():
 
 
 						if str(page) == str(pageCount):
-							url = pageurl
+							url = pageURL
 							headers = {
 							'cache-control': "no-cache",
 							'User-Agent': 'Mozilla/5.0',
